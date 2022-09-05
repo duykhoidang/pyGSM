fd0a05cc958b606bd9866ec156d7f213429fd5ca
Duy-Khoi T Dang <khoidang@umich.edu>
2022-09-05 19:32:04 -0400

    Modified LOT files for ORNL collaboration

--- pygsm/level_of_theories/molpro.py
+++ pygsm/level_of_theories/molpro.py
@@ -3,45 +3,58 @@ import sys
 import os
 from os import path
 import re
-import subprocess
 
 # third party
 import numpy as np
 
 # local application imports
-sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-
-from utilities import manage_xyz
+sys.path.append(path.dirname( path.dirname( path.abspath(__file__))))
 try:
     from .base_lot import Lot
 except:
     from base_lot import Lot
-
+from utilities import *
+import subprocess 
 
 class Molpro(Lot):
 
-    def __init__(self, options):
-        super(Molpro, self).__init__(options)
+    def __init__(self,options):
+        super(Molpro,self).__init__(options)
 
-        self.file_options.set_active('basis', '6-31g', str, '')
-        self.file_options.set_active('closed', None, int, '')
-        self.file_options.set_active('occ', None, int, '')
-        self.file_options.set_active('n_electrons', None, int, '')
-        self.file_options.set_active('memory', 800, int, '')
-        self.file_options.set_active('n_states', 2, int, '')
+        self.file_options.set_active('basis','6-31g',str,'')
+        self.file_options.set_active('closed',None,int,'')
+        self.file_options.set_active('occ',None,int,'')
+        self.file_options.set_active('n_electrons',None,int,'')
+        self.file_options.set_active('memory',800,int,'')
+        self.file_options.set_active('n_states',2,int,'')
+        self.file_options.set_active('restarted',False,bool,'')
 
         # set all active values to self for easy access
         for key in self.file_options.ActiveOptions:
             setattr(self, key, self.file_options.ActiveOptions[key])
 
-        if self.has_nelectrons is False:
+        if self.has_nelectrons==False:
             for i in self.states:
-                self.get_nelec(self.geom, i[0])
+                self.get_nelec(self.geom,i[0])
 
-    def write_input_file(self, tempfilename):
-        # TODO gopro needs a number
-        tempfile = open(tempfilename, 'w')
-        tempfile.write(' file,2,mp_{:04d}_{:04d}\n'.format(self.ID, self.node_id))
+        self.closed = self.options['closed']
+        self.occ = self.options['occ']
+        self.n_electrons = self.options['n_electrons']
+        self.basis = self.options['basis']
+        self.n_states = self.options['n_states']
+        self.restart = self.options['restarted']
+        
+        if self.node_id == 0 and not self.restart:
+            os.system("cp scratch/starter.wfu scratch/mp_{:04d}_{:04d}".format(self.ID,self.node_id))
+
+    # designed to do multiple multiplicities at once... maybe not such a good idea, that feature is currently broken
+    #TODO 
+    def run(self,geom):
+
+        #TODO gopro needs a number
+        tempfilename = 'scratch/gopro.com'
+        tempfile = open(tempfilename,'w')
+        tempfile.write(' file,2,mp_{:04d}_{:04d}\n'.format(self.ID,self.node_id))
         tempfile.write(' memory,{},m\n'.format(self.memory))
         tempfile.write(' symmetry,nosym\n')
         tempfile.write(' orient,noorient\n\n')
@@ -53,160 +66,183 @@ class Molpro(Lot):
         tempfile.write('}\n\n')
 
         # get states
-        singlets = self.search_tuple(self.states, 1)
-        len_singlets = len(singlets)
-        # len_E_singlets = singlets[-1][1] + 1  # final adiabat +1 because 0 indexed
-        triplets = self.search_tuple(self.states, 3)
-        len_triplets = len(triplets)
+        singlets=self.search_tuple(self.states,1)
+        len_singlets=len(singlets)
+        len_E_singlets=singlets[-1][1] +1 #final adiabat +1 because 0 indexed
+        triplets=self.search_tuple(self.states,3)
+        len_triplets=len(triplets)
 
         tempfile.write(' basis={}\n\n'.format(self.basis))
+        tempfile.write('gthresh,orbital=1.d-7,step=5.d-6\n\n')
 
         # do singlets
-        if len_singlets != 0:
+        if len_singlets is not 0:
             tempfile.write(' {multi\n')
+            tempfile.write(' start,2140.2\n')
+            tempfile.write(' orbital,2140.2\n')
             tempfile.write(' direct\n')
+            tempfile.write(' maxiter,21\n')
             tempfile.write(' closed,{}\n'.format(self.closed))
             tempfile.write(' occ,{}\n'.format(self.occ))
             tempfile.write(' wf,{},1,0\n'.format(self.n_electrons))
-            # this can be made the len of singlets
+            #this can be made the len of singlets
             tempfile.write(' state,{}\n'.format(self.n_states))
 
             for state in self.gradient_states:
-                s = state[1]
-                print("running grad state ", s)
-                grad_name = "510"+str(s)+".1"
-                tempfile.write(' CPMCSCF,GRAD,{}.1,record={}\n'.format(s+1, grad_name))
+                s=state[1]
+                print("running grad state ",s)
+                grad_name="510"+str(s)+".1"
+                tempfile.write(' CPMCSCF,GRAD,{}.1,record={}\n'.format(s+1,grad_name))
 
-            # TODO this can only do coupling if states is 2, want to generalize to 3 states
+            #TODO this can only do coupling if states is 2, want to generalize to 3 states
             if self.coupling_states:
-                tempfile.write('CPMCSCF,NACM,{}.1,{}.1,record=5200.1\n'.format(self.coupling_states[0], self.coupling_states[1]))
+                tempfile.write('CPMCSCF,NACM,{}.1,{}.1,record=5200.1\n'.format(self.coupling_states[0]+1,self.coupling_states[1]+1))
             tempfile.write(' }\n')
 
             for state in self.gradient_states:
-                s = state[1]
-                grad_name = "510"+str(s)+".1"
+                s=state[1]
+                grad_name="510"+str(s)+".1"
                 tempfile.write('Force;SAMC,{};varsav\n'.format(grad_name))
             if self.coupling_states:
                 tempfile.write('Force;SAMC,5200.1;varsav\n')
 
-        if len_triplets != 0:
+        if len_triplets is not 0:
             tempfile.write(' {multi\n')
             nclosed = self.nocc
             nocc = nclosed+self.nactive
             tempfile.write(' closed,{}\n'.format(nclosed))
             tempfile.write(' occ,{}\n'.format(nocc))
             tempfile.write(' wf,{},1,2\n'.format(self.n_electrons))
-            # nstates = len(self.states)
+            nstates = len(self.states)
             tempfile.write(' state,{}\n'.format(len_triplets))
 
             for state in triplets:
-                s = state[1]
-                grad_name = "511"+str(s)+".1"
-                tempfile.write(' CPMCSCF,GRAD,{}.1,record={}\n'.format(s+1, grad_name))
+                s=state[1]
+                grad_name="511"+str(s)+".1"
+                tempfile.write(' CPMCSCF,GRAD,{}.1,record={}\n'.format(s+1,grad_name))
             tempfile.write(' }\n')
 
             for s in triplets:
-                s = state[1]
-                grad_name = "511"+str(s)+".1"
+                s=state[1]
+                grad_name="511"+str(s)+".1"
                 tempfile.write('Force;SAMC,{};varsav\n'.format(grad_name))
 
         tempfile.close()
+        scratch = os.environ['SLURM_LOCAL_SCRATCH']
 
-    # designed to do multiple multiplicities at once... maybe not such a good idea, that feature is currently broken
-    # TODO
-    def runall(self, geom, runtype=None):
-
-        self.Gradients = {}
-        self.Energies = {}
-        self.Couplings = {}
-        tempfilename = 'scratch/{}/gopro.com'.format(self.node_id)
-        try:
-            scratch = os.environ['SLURM_LOCAL_SCRATCH']
-            args = ['-W', 'scratch', '-n', str(self.nproc), tempfilename, '-d', scratch]
-        except:
-            args = [os.environ['MOLPRO_OPTIONS'], '-n', str(self.nproc), tempfilename]
-
-        print(args)
-
-        # WRite input file
-        self.write_input_file(tempfilename)
-
-        # Run Process
-        command = ['molpro']
+        #cmd = "molpro -W scratch -n {} {} -d {} --no-xml-output".format(self.nproc,tempfilename,scratch)
+        args = ['-W','scratch','-n',str(self.nproc),tempfilename,'-d',scratch]
+        command=['molpro']
         command.extend(args)
-        output = subprocess.Popen(command, stdout=open('scratch/{}/gopro.out'.format(self.node_id), 'w'), stderr=subprocess.PIPE).communicate()
-        print(output[0])
+        output = subprocess.Popen(command, stdout=subprocess.PIPE, stderr = subprocess.PIPE).communicate()[0]
 
-        # Parse
-        self.parse()
-        self.write_E_to_file()
+        #with open('molpro.stdout','a') as out:
+        #    with nifty.custom_redirection2(out):
+        #        os.system(cmd)
+        #process = Popen(['cat', 'test.py'], stdout=PIPE, stderr=PIPE)
+        #process = Popen([cmd], stdout=PIPE, stderr=PIPE)
+        #stdout, stderr = process.communicate()
 
-        self.hasRanForCurrentCoords = True
-        return
-
-    def parse(self):
         # Now read the output
-        tempfileout = 'scratch/{}/gopro.out'.format(self.node_id)
+        tempfileout='scratch/gopro.out'
         pattern = re.compile(r'MCSCF STATE \d.1 Energy \s* ([-+]?[0-9]*\.?[0-9]+)')
-        tmp = []
-        for i, line in enumerate(open(tempfileout)):
-            for match in re.finditer(pattern, line):
+        self.E = []
+        tmp =[]
+        for i,line in enumerate(open(tempfileout)):
+            for match in re.finditer(pattern,line):
                 tmp.append(float(match.group(1)))
 
-        for state, E in zip(self.states, tmp):
-            self._Energies[state] = self.Energy(E, 'Hartree')
+        for i in range(len_E_singlets):
+            self.E.append((1,i,tmp[i]))
+        for i in range(len_triplets): #triplets broken here
+            self.E.append((3,i,tmp[len_singlets+i]))
 
-        tmpgrada = []
-        tmpgrad = []
-        tmpcoup = []
-        with open(tempfileout, "r") as f:
+        tmpgrada=[]
+        tmpgrad=[]
+        self.grada=[]
+        self.coup=[]
+        with open(tempfileout,"r") as f:
             for line in f:
-                if line.startswith("GRADIENT FOR STATE", 7):  # will work for SA-MC and RSPT2 HF
+                if line.startswith("GRADIENT FOR STATE",7): #will work for SA-MC and RSPT2 HF
                     for i in range(3):
                         next(f)
                     for i in range(len(geom)):
-                        findline = next(f, '').strip()
+                        findline = next(f,'').strip()
                         mobj = re.match(r'^\s*(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s*$', findline)
                         tmpgrad.append([
                             float(mobj.group(2)),
                             float(mobj.group(3)),
                             float(mobj.group(4)),
-                        ])
+                            ])
                     tmpgrada.append(tmpgrad)
                     tmpgrad = []
                 if line.startswith(" SA-MC NACME FOR STATES"):
                     for i in range(3):
                         next(f)
                     for i in range(len(geom)):
-                        findline = next(f, '').strip()
+                        findline = next(f,'').strip()
                         mobj = re.match(r'^\s*(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s*$', findline)
-                        tmpcoup.append([
+                        self.coup.append([
                             float(mobj.group(2)),
                             float(mobj.group(3)),
                             float(mobj.group(4)),
-                        ])
-                    self.Couplings[self.coupling_states] = self.Coupling(np.asarray(tmpcoup), "Hartree/Bohr")
-        for state, grad in zip(self.states, tmpgrada):
-            self._Gradients[state] = self.Gradient(np.asarray(grad), "Hartree/Bohr")
+                            ])
+
+        for count,i in enumerate(self.gradient_states):
+            if i[0]==1:
+                self.grada.append((1,i[1],tmpgrada[count]))
+            if i[0]==3:
+                self.grada.append((3,i[1],tmpgrada[count]))
+
+        with open('scratch/E_{}.txt'.format(self.node_id),'w') as f:
+            for E in self.E:
+                f.write('{} {} {:9.7f}\n'.format(E[0],E[1],E[2]))
+        self.hasRanForCurrentCoords=True
+        return
+
+    def get_energy(self,coords,multiplicity,state):
+        if self.hasRanForCurrentCoords==False or (coords != self.currentCoords).any():
+            self.currentCoords = coords.copy()
+            geom = manage_xyz.np_to_xyz(self.geom,self.currentCoords)
+            self.run(geom)
+        return self.search_PES_tuple(self.E,multiplicity,state)[0][2]*units.KCAL_MOL_PER_AU
+
+    def get_gradient(self,coords,multiplicity,state):
+        if self.hasRanForCurrentCoords==False or (coords != self.currentCoords).any():
+            self.currentCoords = coords.copy()
+            geom = manage_xyz.np_to_xyz(self.geom,self.currentCoords)
+            self.run(geom)
+        tmp = self.search_PES_tuple(self.grada,multiplicity,state)
+        print("getting gradient: mult: {} state: {}".format(multiplicity, state))
+#        print(self.grada)
+#        print(tmp)
+        tmp = tmp[0][2]
+        return np.asarray(tmp)*units.ANGSTROM_TO_AU  #hartree/ang
+
+    def get_coupling(self,coords,multiplicity,state1,state2):
+        if self.hasRanForCurrentCoords==False or (coords != self.currentCoords).any():
+            self.currentCoords = coords.copy()
+            geom = manage_xyz.np_to_xyz(self.geom,self.currentCoords)
+            self.run(geom)
+        return np.reshape(self.coup,(3*len(self.coup),1))*units.ANGSTROM_TO_AU
 
     @classmethod
-    def copy(cls, lot, options, copy_wavefunction=True):
+    def copy(cls,lot,options,copy_wavefunction=True):
         """ create a copy of this lot object"""
-        # print(" creating copy, new node id =",node_id)
-        # print(" old node id = ",self.node_id)
-        node_id = options.get('node_id', 1)
-        if node_id != lot.node_id and copy_wavefunction:
-            cmd = "cp scratch/mp_{:04d}_{:04d} scratch/mp_{:04d}_{:04d}".format(lot.ID, lot.node_id, lot.ID, node_id)
+        #print(" creating copy, new node id =",node_id)
+        #print(" old node id = ",self.node_id)
+        node_id = options.get('node_id',1)
+        if node_id != lot.node_id and copy_wavefunction and not lot.restart:
+            cmd = "cp scratch/mp_{:04d}_{:04d} scratch/mp_{:04d}_{:04d}".format(lot.ID,lot.node_id,lot.ID,node_id)
             print(cmd)
             os.system(cmd)
         return cls(lot.options.copy().set_values(options))
 
-
-if __name__ == '__main__':
-    filepath = "../../data/ethylene.xyz"
-    molpro = Molpro.from_options(states=[(1, 0), (1, 1)], fnm=filepath, lot_inp_file='../../data/ethylene_molpro.com', coupling_states=(0, 1))
-    geom = manage_xyz.read_xyz(filepath)
+if __name__=='__main__':
+    filepath="../../data/ethylene.xyz"
+    molpro = Molpro.from_options(states=[(1,0),(1,1)],fnm=filepath,lot_inp_file='../../data/ethylene_molpro.com',coupling_states=[0,1])
+    geom=manage_xyz.read_xyz(filepath)
     xyz = manage_xyz.xyz_to_np(geom)
-    print(molpro.get_energy(xyz, 1, 0))
-    print(molpro.get_gradient(xyz, 1, 0))
-    print(molpro.get_coupling(xyz, 1, 0, 1))
+    print(molpro.get_energy(xyz,1,0))
+    print(molpro.get_gradient(xyz,1,0))
+    print(molpro.get_coupling(xyz,1,0,1))
--- pygsm/level_of_theories/qchem.py
+++ pygsm/level_of_theories/qchem.py
@@ -1,5 +1,4 @@
 # standard library imports
-import subprocess
 import sys
 import os
 from os import path
@@ -8,29 +7,30 @@ from os import path
 import numpy as np
 
 # local application imports
-sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+sys.path.append(path.dirname( path.dirname( path.abspath(__file__))))
 from .base_lot import Lot
-
+from utilities import *
+import subprocess 
 
 class QChem(Lot):
-    def __init__(self, options):
-        super(QChem, self).__init__(options)
+    def __init__(self,options):
+        super(QChem,self).__init__(options)
 
         qcscratch = os.environ['QCSCRATCH']
         for state in self.states:
-            tempfolder = qcscratch + '/string_{:03d}/{}.{}/'.format(self.ID, self.node_id, state[0])
+            tempfolder = qcscratch + '/string_{:03d}/{}.{}/'.format(self.ID,self.node_id,state[0])
             print(" making temp folder {}".format(tempfolder))
             os.system('mkdir -p {}'.format(tempfolder))
 
         copy_input_file = os.getcwd() + "/QChem_input.txt"
         print(copy_input_file)
-        self.write_preamble(self.geom, self.states[0][0], copy_input_file)
+        self.write_preamble(self.geom,self.states[0][0],copy_input_file)
         print(" making folder scratch/{}".format(self.node_id))
         os.system('mkdir -p scratch/{}'.format(self.node_id))
 
-    def write_preamble(self, geom, multiplicity, tempfilename, jobtype='FORCE'):
+    def write_preamble(self,geom,multiplicity,tempfilename,jobtype='FORCE'):
 
-        tempfile = open(tempfilename, 'w')
+        tempfile = open(tempfilename,'w')
         if not self.lot_inp_file:
             tempfile.write(' $rem\n')
             tempfile.write(' JOBTYPE {}\n'.format(jobtype))
@@ -38,7 +38,7 @@ class QChem(Lot):
             tempfile.write(' SCF_ALGORITHM rca_diis\n')
             tempfile.write(' SCF_MAX_CYCLES 300\n')
             tempfile.write(' BASIS {}\n'.format(self.basis))
-            # tempfile.write(' ECP LANL2DZ \n')
+            #tempfile.write(' ECP LANL2DZ \n')
             tempfile.write(' WAVEFUNCTION_ANALYSIS FALSE\n')
             tempfile.write(' GEOM_OPT_MAX_CYCLES 300\n')
             tempfile.write('scf_convergence 6\n')
@@ -54,12 +54,12 @@ class QChem(Lot):
             for line in lot_inp_lines:
                 tempfile.write(line)
 
-        tempfile.write('{} {}\n'.format(self.charge, multiplicity))
+        tempfile.write('{} {}\n'.format(self.charge,multiplicity))
         if os.path.isfile("link.txt"):
             with open("link.txt") as link:
                 link_lines = link.readlines()
             tmp_geom = [list(i) for i in geom]
-            for i, coord in enumerate(tmp_geom):
+            for i,coord in enumerate(tmp_geom):
                 coord.append(link_lines[i].rstrip('\n'))
                 for i in coord:
                     tempfile.write(str(i)+' ')
@@ -71,47 +71,41 @@ class QChem(Lot):
                 tempfile.write('\n')
         tempfile.write('$end')
         tempfile.close()
-
-    def run(self, geom, multiplicity, ad_idx, runtype='gradient'):
-
-        assert ad_idx == 0, "pyGSM Q-Chem doesn't currently support ad_idx!=0"
+    
+    def run(self,geom,multiplicity):
 
         qcscratch = os.environ['QCSCRATCH']
-        tempfilename = qcscratch + '/string_{:03d}/{}.{}/tempQCinp'.format(self.ID, self.node_id, multiplicity)
+        tempfilename = qcscratch + '/string_{:03d}/{}.{}/tempQCinp'.format(self.ID,self.node_id,multiplicity)
 
-        if self.calc_grad and runtype != "energy":
-            self.write_preamble(geom, multiplicity, tempfilename)
+        if self.calc_grad:
+           self.write_preamble(geom,multiplicity,tempfilename)
         else:
-            self.write_preamble(geom, multiplicity, tempfilename, jobtype='SP')
+           self.write_preamble(geom,multiplicity,tempfilename,jobtype='SP')
+        
+        #cmd = "qchem -nt {} -save {} {}.qchem.out string_{:03d}/{}.{}".format(self.nproc,tempfilename,tempfilename,self.ID,self.node_id,multiplicity)
 
         cmd = ['qchem']
-        args = ['-nt', str(self.nproc),
+        args = ['-nt',str(self.nproc),
                 '-save',
                 tempfilename,
                 '{}.qchem.out'.format(tempfilename),
-                'string_{:03d}/{}.{}'.format(self.ID, self.node_id, multiplicity)
+                'string_{:03d}/{}.{}'.format(self.ID,self.node_id,multiplicity)
                 ]
         cmd.extend(args)
-
-        #  Run the process
-        subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
-
-        self.parse(qcscratch, multiplicity)
-
-        return
-
-    def parse(self, qcscratch, multiplicity):
+        output = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr = subprocess.PIPE).communicate()[0]
+        #print(cmd)
+        #os.system(cmd)
+       
         # PARSE OUTPUT #
         if self.calc_grad:
-            efilepath = qcscratch + '/string_{:03d}/{}.{}/GRAD'.format(self.ID, self.node_id, multiplicity)
+            efilepath = qcscratch + '/string_{:03d}/{}.{}/GRAD'.format(self.ID,self.node_id,multiplicity)
             with open(efilepath) as efile:
                 elines = efile.readlines()
-
+            
             temp = 0
             for lines in elines:
                 if temp == 1:
-                    # defaulting to the ground-state
-                    self._Energies[(multiplicity, 0)] = self.Energy(float(lines.split()[0]), 'Hartree')
+                    self.E.append((multiplicity,float(lines.split()[0])))
                     break
                 if "$" in lines:
                     temp += 1
@@ -119,31 +113,69 @@ class QChem(Lot):
             with open(efilepath) as efile:
                 gradlines = efile.readlines()
             temp = 0
-            tmp = []
+            tmp=[]
             for lines in gradlines:
                 if '$' in lines:
-                    temp += 1
+                    temp+=1
                 elif temp == 2:
                     tmpline = lines.split()
                     tmp.append([float(i) for i in tmpline])
                 elif temp == 3:
                     break
-            self._Gradients[(multiplicity, 0)] = self.Gradient(np.asarray(tmp), 'Hartree/Bohr')
+            self.grada.append((multiplicity,tmp))
         else:
             raise NotImplementedError
-        self.write_E_to_file()
+
+        # write E to scratch
+        with open('scratch/E_{}.txt'.format(self.node_id),'w') as f:
+            for E in self.E:
+                f.write('{} {:9.7f}\n'.format(E[0],E[1]))
+
+
+        return 
+
+    def get_energy(self,coords,multiplicity,state):
+        #if self.has_nelectrons==False:
+        #    for i in self.states:
+        #        self.get_nelec(geom,i[0])
+        #    self.has_nelectrons==True
+        if self.hasRanForCurrentCoords==False or (coords != self.currentCoords).any():
+            #print(" Running calculation!")
+            #print(coords.flatten())
+            self.currentCoords = coords.copy()
+            geom = manage_xyz.np_to_xyz(self.geom,self.currentCoords)
+            #print(geom)
+            self.runall(geom)
+            self.hasRanForCurrentCoords=True
+        #else:
+        #    print(" Returning memoization!")
+        tmp = self.search_tuple(self.E,multiplicity)
+        return np.asarray(tmp[state][1])*units.KCAL_MOL_PER_AU
+
+    def get_gradient(self,coords,multiplicity,state):
+        #if self.has_nelectrons==False:
+        #    for i in self.states:
+        #        self.get_nelec(geom,i[0])
+        #    self.has_nelectrons==True
+        if self.hasRanForCurrentCoords==False or (coords != self.currentCoords).any():
+            self.currentCoords = coords.copy()
+            geom = manage_xyz.np_to_xyz(self.geom,self.currentCoords)
+            self.runall(geom)
+        tmp = self.search_tuple(self.grada,multiplicity)
+        return np.asarray(tmp[state][1])*units.ANGSTROM_TO_AU
 
     @classmethod
-    def copy(cls, lot, options, copy_wavefunction=True):
+    def copy(cls,lot,options,copy_wavefunction=True):
         base = os.environ['QCSCRATCH']
-        node_id = options.get('node_id', 1)
+        node_id = options.get('node_id',1)
 
-        if node_id != lot.node_id:  # and copy_wavefunction: # other theories are more sensitive than qchem -- commenting out
+        if node_id != lot.node_id:  #and copy_wavefunction: # other theories are more sensitive than qchem -- commenting out
             for state in lot.states:
                 multiplicity = state[0]
-                efilepath_old = base + '/string_{:03d}/{}.{}'.format(lot.ID, lot.node_id, multiplicity)
-                efilepath_new = base + '/string_{:03d}/{}.{}'.format(lot.ID, node_id, multiplicity)
-                cmd = 'cp -r ' + efilepath_old + ' ' + efilepath_new
+                efilepath_old=base+ '/string_{:03d}/{}.{}'.format(lot.ID,lot.node_id,multiplicity)
+                efilepath_new =base+ '/string_{:03d}/{}.{}'.format(lot.ID,node_id,multiplicity)
+                cmd = 'cp -r ' + efilepath_old +' ' + efilepath_new
                 print(" copying QCSCRATCH files\n {}".format(cmd))
                 os.system(cmd)
         return cls(lot.options.copy().set_values(options))
+
